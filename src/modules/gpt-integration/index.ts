ea7d7055b1847377841ae878f5d7ae7c961dca10
brenodanyel (idz) <bdls1704@hotmail.com>
2024-03-10 13:03:28 -0300

    update

--- src/modules/gpt-integration/index.ts
+++ src/modules/gpt-integration/index.ts
@@ -1,7 +1,7 @@
 import * as Discord from "discord.js";
 import { Module } from "../../core/module";
-import { Imagine } from "./slash-commands/imagine";
-import { Welcome } from "./static-messages/welcome";
+import { Imagine } from "./slash-commands/imagine.command";
+import { Welcome } from "./static-messages/welcome.static-message";
 
 export class GPTIntegrationModule extends Module {
   constructor(bot: Discord.Client<true>) {
