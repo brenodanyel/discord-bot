8456bab2d8224b5909fc163b31ed6177ab79d9b8
brenodanyel (idz) <bdls1704@hotmail.com>
2024-03-10 14:17:10 -0300

    update

--- src/modules/gpt-integration/static-messages/welcome.static-message.ts
+++ src/modules/gpt-integration/static-messages/welcome.static-message.ts
@@ -1,14 +1,14 @@
 import { BaseMessageOptions, ButtonBuilder, ButtonStyle, ComponentType, EmbedBuilder } from "discord.js";
 import { StaticMessage } from "../../../core/static-message";
 
-const embed = new EmbedBuilder() //
+const embed = new EmbedBuilder()
   .setTitle("Bem-vindo ao servidor!")
   .setAuthor({ name: "GPT Integration", iconURL: "https://i.imgur.com/3tj5J8J.png" })
   .setDescription("Este é um servidor de teste para o módulo de integração com GPT")
   .setColor("#FF0000")
   .setTimestamp();
 
-const component = new ButtonBuilder() //
+const component = new ButtonBuilder()
   .setCustomId("welcome-button")
   .setLabel("Clique aqui para começar3")
   .setEmoji("👋")
@@ -19,7 +19,6 @@ export class WelcomeStaticMessage extends StaticMessage {
   existingMessageId = "1216432111997685982";
 
   message: BaseMessageOptions = {
-    content: "Bem-vindo ao servidor!",
     embeds: [embed],
     components: [
       {
